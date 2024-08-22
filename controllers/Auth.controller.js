73a91d348d19f941a8596052d3c6899ce24f3b55
Ayush Singh <116483034+ayush-singh-dev@users.noreply.github.com>
2024-08-22 22:59:34 +0530

    Update Auth.controller.js

--- controllers/Auth.controller.js
+++ controllers/Auth.controller.js
@@ -44,7 +44,7 @@ const login = async (req, res) => {
       const options = {
         httpOnly: true,
         secure: true,
-        sameSite: "Lax",
+        sameSite: "None",
       };
       return res
         .status(200)
