a7c9dd1132eb287d9683248b7b16414a946efb6a
Ayush Singh <116483034+ayush-singh-dev@users.noreply.github.com>
2024-08-22 22:04:53 +0530

    Update index.js localhost

--- index.js
+++ index.js
@@ -16,7 +16,8 @@ app.get("/home", (req, res) => {
 });
 
 const corsOptions = {
-  origin: "https://dear-diary-swart.vercel.app",
+  // origin: "https://dear-diary-swart.vercel.app",
+  origin: "http://localhost:5173/"
   methods: "GET, PUT , POST , DELETE, PATCH, HEAD",
   credentials: true,
 };
