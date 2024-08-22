437c8e2f9806359d90d8e9262eb36baba6a1948c
Ayush Singh <116483034+ayush-singh-dev@users.noreply.github.com>
2024-08-22 22:06:43 +0530

    Update index.js localhost with error resoved.

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.get("/home", (req, res) => {
 
 const corsOptions = {
   // origin: "https://dear-diary-swart.vercel.app",
-  origin: "http://localhost:5173/"
+  origin: "http://localhost:5173/",
   methods: "GET, PUT , POST , DELETE, PATCH, HEAD",
   credentials: true,
 };
